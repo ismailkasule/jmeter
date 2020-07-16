2fdc6ccb270e9bd69e83429164c6ddaf52bdada8
ismailkasule <ikasule@yahoo.com>
2020-07-16 09:47:20 +0200

    Changed test script file again

--- jmeter/test.sh
+++ jmeter/test.sh
@@ -4,29 +4,25 @@ rootPath=$1
 testFile=$2
 host=$3
 
-echo "Root path: $rootPath"
-echo "Test file: $testFile"
-echo "Host: $host"
-
 T_DIR=.
 
 # Reporting dir: start fresh
 R_DIR=./report
-rm -rf $R_DIR 
-mkdir -p $R_DIR
+rm -rf ./report
+mkdir -p ./report
 
-rm -f $T_DIR/test-plan.jtl $T_DIR/jmeter.log
+rm -f ./test-plan.jtl ./jmeter.log
 
-./run.sh $rootPath -Dlog_level.jmeter=DEBUG \
-	-Jhost=$host \
-	-n -t /test/$testFile -l $T_DIR/test-plan.jtl -j $T_DIR/jmeter.log \
-	-e -o $R_DIR
+./run.sh /jmeter/jmeter -Dlog_level.jmeter=DEBUG \
+	-Jhost=dev-riskmarketplace.standardbank.co.za \
+	-n -t /test/RMP_CreateIncident.jmx -l ./test-plan.jtl -j ./jmeter.log \
+	-e -o ./report
 
 echo "==== jmeter.log ===="
-cat $T_DIR/jmeter.log
+cat ./jmeter.log
 
 echo "==== Raw Test Report ===="
-cat $T_DIR/test-plan.jtl
+cat ./test-plan.jtl
 
 echo "==== HTML Test Report ===="
 echo "See HTML test report in $R_DIR/index.html"f
